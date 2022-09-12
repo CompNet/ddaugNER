f00a0115ef8cfff9cf17817eae129f794c8f2e42
Aethor <oow.autre@gmail.com>
2022-09-12 11:44:12 +0200

    Add lexical overlap computation script

--- lexical_overlap.py
+++ lexical_overlap.py
@@ -1,13 +1,13 @@
-import re, glob, os, json, copy, rich
+import os, json, copy
 import nltk
-from typing import List, Set, Tuple
+from typing import Dict, List, Set, Tuple
 import functools
 import pandas as pd
-import numpy as np
 from nltk.corpus import stopwords
 from tqdm import tqdm
 from nameparser.config.titles import TITLES
 from transformers import BertTokenizer
+from rich import print
 from ddaugner.datas.conll import CoNLLDataset
 from ddaugner.utils import entities_from_bio_tags, flattened
 from ddaugner.datas.dekker import load_dekker_dataset
@@ -16,40 +16,9 @@ from ddaugner.datas.dekker import load_dekker_dataset
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-dekker_dataset = load_dekker_dataset("./ner", "fantasy")
-dekker_entities = entities_from_bio_tags(
-    flattened([s.tokens for s in dekker_dataset.sents]),
-    flattened([s.tags for s in dekker_dataset.sents]),
-)
-dekker_names = set([tuple(e.tokens) for e in dekker_entities])
-
-conll_dataset = CoNLLDataset.train_dataset({}, {})
-conll_entities = entities_from_bio_tags(
-    flattened([s.tokens for s in conll_dataset.sents]),
-    flattened([s.tags for s in conll_dataset.sents]),
-)
-conll_names = set([tuple(e.tokens) for e in conll_entities if e.tag == "PER"])
-
-
-with open(f"{script_dir}/ddaugner/resources/wgold.json") as f:
-    wgold_names = json.load(f)
-# TODO
-wgold_names = set([tuple([t for t in name.split(" ") if t]) for name in wgold_names])
-
-with open(f"{script_dir}/ddaugner/resources/morrowind_names.json") as f:
-    morrowind_names = json.load(f)
-morrowind_names = set(
-    [tuple([t for t in name.split(" ") if t]) for name in morrowind_names]
-)  # TODO
-
-
-def exact_match(
-    nameset1: Set[Tuple[str, ...]], nameset2: Set[Tuple[str, ...]]
-) -> Set[Tuple[str, ...]]:
+def exact_match(nameset1: Set[str], nameset2: Set[str]) -> Set[str]:
     """Compute the proportion of mentions of ``nameset1`` that have an
     exact match in ``nameset2``
-
-    :return: a `float` between 0 and 1
     """
     return {name for name in nameset1 if name in nameset2}
 
@@ -65,68 +34,36 @@ def is_stopword(token: str) -> bool:
     return token in stopwords.words("english")
 
 
-def partial_match(
-    nameset1: Set[Tuple[str, ...]], nameset2: Set[Tuple[str, ...]]
-) -> Set[Tuple[str, ...]]:
-    """Compute the proportion of mentions of ``nameset1`` that have a
-    partial match in ``nameset2``
-
-    .. note::
-
-        titles are *not* taken into account when partial matching
-
-    :return: a `float` between 0 and 1
-    """
-
-    def names_partially_match(name1: Tuple[str, ...], name2: Tuple[str, ...]) -> bool:
-        for tok in name1:
-            if len(name1) > 1 and is_title(tok):
-                continue
-            if len(name1) > 1 and is_stopword(tok):
-                continue
-            if tok in name2:
-                return True
-        return False
-
-    return {
-        name1
-        for name1 in nameset1
-        if any([names_partially_match(name1, name2) for name2 in nameset2])
-    }
+tokenizer = None
 
 
-def wp_partial_match(
-    nameset1: Set[Tuple[str, ...]], nameset2: Set[Tuple[str, ...]]
-) -> Set[Tuple[str, ...]]:
+def get_tokenizer() -> BertTokenizer:
+    global tokenizer
+    if tokenizer is None:
+        tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+    return tokenizer
+
+
+@functools.lru_cache(maxsize=None)
+def wp_tokenize(name: str) -> List[str]:
+    tokenizer = get_tokenizer()
+    return tokenizer.tokenize(name)
+
+
+def wp_partial_match(nameset1: Set[str], nameset2: Set[str]) -> Set[str]:
     """Compute the proportion of mentions of ``nameset1`` that have a
     partial match in ``nameset2``, taking wordpiece"""
-    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 
-    @functools.lru_cache(maxsize=None)
-    def subtokenize(name: Tuple[str, ...]) -> Tuple[str, ...]:
-        """HACK"""
-        return tuple(
-            [
-                tokenizer.decode([e])
-                for e in tokenizer.encode(
-                    name, is_split_into_words=True, add_special_tokens=False
-                )
-            ]
-        )
+    def names_partially_match(name1: str, name2: str) -> bool:
 
-    def names_partially_match(name1: Tuple[str, ...], name2: Tuple[str, ...]) -> bool:
-
-        if len(name1) > 1:
-            name1 = tuple([t for t in name1 if not is_title(t) and not is_stopword(t)])
-
-        if len(name2) > 1:
-            name2 = tuple([t for t in name2 if not is_title(t) and not is_stopword(t)])
-
-        if len(name1) == 0 or len(name2) == 0:
+        if is_title(name1) or is_title(name2):
             return False
 
-        wp_name1 = subtokenize(name1)
-        wp_name2 = subtokenize(name2)
+        if is_stopword(name1) or is_stopword(name2):
+            return False
+
+        wp_name1 = wp_tokenize(name1)
+        wp_name2 = wp_tokenize(name2)
 
         for wp1 in wp_name1:
             if wp1 in wp_name2:
@@ -141,112 +78,119 @@ def wp_partial_match(
     }
 
 
-def overlap_subsets(
-    target_set: Set[Tuple[str, ...]], source_set: Set[Tuple[str, ...]]
-) -> dict:
+def overlap_subsets(target_set: Set[str], source_set: Set[str]) -> Dict[str, float]:
 
     new_set = copy.copy(target_set)
     exact_matchs_set = exact_match(new_set, source_set)
     new_set -= exact_matchs_set
-    partial_matchs_set = partial_match(new_set, source_set)
-    new_set -= partial_matchs_set
     wp_partial_matchs_set = wp_partial_match(new_set, source_set)
     new_set -= wp_partial_matchs_set
 
     return {
         "exact matchs": len(exact_matchs_set) / len(target_set),
-        "partial matchs": len(partial_matchs_set) / len(target_set),
         "wordpiece partial matchs": len(wp_partial_matchs_set) / len(target_set),
         "new": len(new_set) / len(target_set),
     }
 
 
-namesets = {
-    "dekker": dekker_names,
-    "conll": conll_names,
-    "wgold": wgold_names,
-    "morrowind": morrowind_names,
-}
+if __name__ == "__main__":
 
-overlaps = {}
-for nameset1, names1 in tqdm(namesets.items()):
-    for nameset2, names2 in namesets.items():
-        overlaps[f"{nameset1} {nameset2}"] = overlap_subsets(names1, names2)
+    dekker_dataset = load_dekker_dataset("./ner", "fantasy")
+    dekker_entities = entities_from_bio_tags(
+        flattened([s.tokens for s in dekker_dataset.sents]),
+        flattened([s.tags for s in dekker_dataset.sents]),
+    )
+    dekker_names = set(flattened([e.tokens for e in dekker_entities]))
 
+    conll_dataset = CoNLLDataset.train_dataset({}, {})
+    conll_entities = entities_from_bio_tags(
+        flattened([s.tokens for s in conll_dataset.sents]),
+        flattened([s.tags for s in conll_dataset.sents]),
+    )
+    conll_names = set(flattened([e.tokens for e in conll_entities if e.tag == "PER"]))
 
-# rich.print(overlaps)
-
-
-rows = []
-
-for nameset in ("conll", "wgold", "morrowind"):
-    # rows.append(
-    #     pd.Series(
-    #         {
-    #             "exact matchs": overlaps[f"{nameset} dekker"]["exact matchs"],
-    #             "partial matchs": overlaps[f"{nameset} dekker"]["partial matchs"],
-    #             "wordpiece partial matchs": overlaps[f"{nameset} dekker"][
-    #                 "wordpiece partial matchs"
-    #             ],
-    #             "new": overlaps[f"{nameset} dekker"]["new"],
-    #         },
-    #         name=nameset,
-    #     )
-    # )
-    rows.append(
-        pd.Series(
-            {
-                "exact matchs": overlaps[f"dekker {nameset}"]["exact matchs"],
-                "partial matchs": overlaps[f"dekker {nameset}"]["partial matchs"],
-                "wordpiece partial matchs": overlaps[f"dekker {nameset}"][
-                    "wordpiece partial matchs"
-                ],
-                "new": overlaps[f"dekker {nameset}"]["new"],
-            },
-            name=nameset,
-        )
+    with open(f"{script_dir}/ddaugner/resources/wgold.json") as f:
+        wgold_names = json.load(f)
+    # TODO
+    wgold_names = set(
+        flattened([([t for t in name.split(" ") if t]) for name in wgold_names])
     )
 
-df = pd.DataFrame(
-    rows, columns=["exact matchs", "partial matchs", "wordpiece partial matchs", "new"]
-)
-print("Overlap with dekker fantasy dataset")
-print(df)
+    with open(f"{script_dir}/ddaugner/resources/morrowind_names.json") as f:
+        morrowind_names = json.load(f)
+    morrowind_names = set(
+        flattened([[t for t in name.split(" ") if t] for name in morrowind_names])
+    )  # TODO
 
-
-rows = []
-
-for nameset in ("conll", "wgold", "morrowind"):
-    # rows.append(
-    #     pd.Series(
-    #         {
-    #             "exact matchs": overlaps[f"{nameset} conll"]["exact matchs"],
-    #             "partial matchs": overlaps[f"{nameset} conll"]["partial matchs"],
-    #             "wordpiece partial matchs": overlaps[f"{nameset} conll"][
-    #                 "wordpiece partial matchs"
-    #             ],
-    #             "new": overlaps[f"{nameset} conll"]["new"],
-    #         },
-    #         name=nameset,
-    #     )
-    # )
-    rows.append(
-        pd.Series(
-            {
-                "exact matchs": overlaps[f"conll {nameset}"]["exact matchs"],
-                "partial matchs": overlaps[f"conll {nameset}"]["partial matchs"],
-                "wordpiece partial matchs": overlaps[f"conll {nameset}"][
-                    "wordpiece partial matchs"
-                ],
-                "new": overlaps[f"conll {nameset}"]["new"],
-            },
-            name=nameset,
+    with open(f"{script_dir}/ddaugner/resources/the_elder_scrolls_names.json") as f:
+        tes_data = json.load(f)
+        tes_names = (
+            tes_data["first_names"]
+            + tes_data["last_names"]
+            + tes_data["suffixs"]
+            + tes_data["prefixs"]
         )
+    # TODO
+    tes_names = set(
+        flattened([[t for t in name.split(" ") if t] for name in tes_names])
     )
 
-df = pd.DataFrame(
-    rows, columns=["exact matchs", "partial matchs", "wordpiece partial matchs", "new"]
-)
+    namesets = {
+        "dekker": dekker_names,
+        "conll": conll_names,
+        "wgold": wgold_names,
+        "morrowind": morrowind_names,
+        "the elder scrolls": tes_names,
+    }
 
-print("Overlap with conll train dataset")
-print(df)
+    overlaps = {}
+    for nameset1, names1 in tqdm(namesets.items()):
+        for nameset2, names2 in tqdm(namesets.items()):
+            overlaps[f"{nameset1} {nameset2}"] = overlap_subsets(names1, names2)
+
+    rows = []
+
+    for nameset in set(namesets.keys()) - {"dekker"}:
+        rows.append(
+            pd.Series(
+                {
+                    "exact matchs": overlaps[f"dekker {nameset}"]["exact matchs"],
+                    "wordpiece partial matchs": overlaps[f"dekker {nameset}"][
+                        "wordpiece partial matchs"
+                    ],
+                    "new": overlaps[f"dekker {nameset}"]["new"],
+                },
+                name=nameset,
+            )
+        )
+
+    df = pd.DataFrame(
+        rows,
+        columns=["exact matchs", "wordpiece partial matchs", "new"],
+    )
+    print("Overlap with dekker fantasy dataset")
+    print(df)
+
+    rows = []
+
+    for nameset in set(namesets.keys()) - {"dekker"}:
+        rows.append(
+            pd.Series(
+                {
+                    "exact matchs": overlaps[f"conll {nameset}"]["exact matchs"],
+                    "wordpiece partial matchs": overlaps[f"conll {nameset}"][
+                        "wordpiece partial matchs"
+                    ],
+                    "new": overlaps[f"conll {nameset}"]["new"],
+                },
+                name=nameset,
+            )
+        )
+
+    df = pd.DataFrame(
+        rows,
+        columns=["exact matchs", "wordpiece partial matchs", "new"],
+    )
+
+    print("Overlap with conll train dataset")
+    print(df)
