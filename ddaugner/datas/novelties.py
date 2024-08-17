24b4e0827b1f83f3ec4407bba3e439f011ede8d6
Aethor <oow.autre@gmail.com>
2024-08-17 16:32:20 +0200

    add nofantasy group to the ddaugner.datas.novelties module

--- ddaugner/datas/novelties.py
+++ ddaugner/datas/novelties.py
@@ -22,7 +22,32 @@ groups = {
         "TheColourOfMagic",
         "TheWayOfKings",
         "TheLiesOfLockeLamora",
-    }
+    },
+    "nofantasy": {
+        "1984",
+        "AliceInWonderland",
+        "AStudyInScarlet",
+        "BraveNewWorld",
+        "DavidCopperfield",
+        "Dracula",
+        "Emma",
+        "Frankenstein",
+        "HarryPotter",
+        "HuckleberryFinn",
+        "JekyllAndHyde",
+        "MobyDick",
+        "OliverTwist",
+        "PrideAndPrejudice",
+        "StormFront",
+        "TheCallOfTheWild",
+        "TheCountOfMonteCristo",
+        "TheGunslinger",
+        "TheThreeMusketeers",
+        "TheWayWeLiveNow",
+        "TinkerTailorSoldierSpy",
+        "Ulysses",
+        "VanityFair",
+    },
 }
 
 
